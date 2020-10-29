3b57d823e0d3428559f23b7944de2d0f41fded5a
Marcos Antônio <marcosfilho1913@hotmail.com>
2020-10-28 23:21:23 -0300

    Subindo segunda versão do sistema

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ def home():
 def login():
     if request.method == 'POST':
         if "submit_button" in request.form:
-            if ((request.form['username'] == 'Xuxinha') and (request.form['password'] == '123456')):
+            if ((request.form['username'] == 'Marcos') and (request.form['password'] == '123456')):
                 print("Login realizado com sucesso!")
                 return render_template("home.html")
             else:
                 return render_template('login.html') 
     return render_template('login.html')
 
-    
+@app.route('/cadastro-livro', methods=['POST', 'GET'])
+def cadastrar_livro():
+    if request.method == 'POST':
+        if "submit_button" in request.form:
+            print("Livro cadastrado com sucesso!")  
+            return render_template('home.html')
+    else:
+        return render_template('cadastro-livro.html')
